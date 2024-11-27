1a8a3e199f95d1095169e32fd70aad1145285f34
JayGhz <jaykansas7@gmail.com>
2024-11-26 23:02:45 -0500

    Refactor entity and DTO files for vets, auth, users, shelters, and customers

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { CustomersModule } from './customers/customers.module';
+import { VetsModule } from './vets/vets.module';
+import { SheltersModule } from './shelters/shelters.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, AuthModule, CustomersModule, VetsModule, SheltersModule],
   controllers: [],
   providers: [],
 })
