6b56a6dd57904c661ded96a6109ff09d525db7d1
JayGhz <jaykansas7@gmail.com>
2024-12-13 20:08:57 -0500

    feat: Refactor authentication module to implement user registration for customers, vets, and shelters; remove unused DTOs and methods

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,26 +1,27 @@
 import { Injectable } from '@nestjs/common';
-import { CreateAuthDto } from './dto/create-auth.dto';
-import { UpdateAuthDto } from './dto/update-auth.dto';
+import { CreateUserDto } from 'src/users/dto/create-user.dto';
+import { User } from 'src/users/entities/user.entity';
+import { UsersService } from 'src/users/users.service';
+
 
 @Injectable()
 export class AuthService {
-  create(createAuthDto: CreateAuthDto) {
-    return 'This action adds a new auth';
+
+  async signIn() {
+    return 'This action logs a user in';
   }
 
-  findAll() {
-    return `This action returns all auth`;
+  constructor(private readonly usersService: UsersService) {}
+
+  async registerCustomer(createUserDto: CreateUserDto): Promise<User> {
+    return await this.usersService.registerCustomer(createUserDto);
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} auth`;
+  async registerVet(createUserDto: CreateUserDto): Promise<User> {
+    return await this.usersService.registerVet(createUserDto);
   }
 
-  update(id: number, updateAuthDto: UpdateAuthDto) {
-    return `This action updates a #${id} auth`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} auth`;
+  async registerShelter(createUserDto: CreateUserDto): Promise<User> {
+    return await this.usersService.registerShelter(createUserDto);
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -16,7 +16,7 @@ import { CreateShelterDto } from 'src/shelters/dto/create-shelter.dto';
 export class UsersService {
 
   constructor(
-    @InjectRepository(User) 
+    @InjectRepository(User)
     private usersRepository: Repository<User>,
     private customerService: CustomersService,
     private vetService: VetsService,
@@ -26,7 +26,7 @@ export class UsersService {
   async create(createUserDto: CreateUserDto, role: Role): Promise<User> {
     const userExists = await this.usersRepository.findOneBy({ email: createUserDto.email });
     if (userExists) {
-      throw new BadRequestException('User already exists');
+      throw new BadRequestException('User with this email already exists');
     }
     const user = this.usersRepository.create({ ...createUserDto, role });
     return await this.usersRepository.save(user);
@@ -34,35 +34,19 @@ export class UsersService {
 
   // Registrar un nuevo Customer 
   async registerCustomer(createUserDto: CreateUserDto): Promise<User> {
-    const user = await this.create(createUserDto, Role.CUSTOMER); 
-    
-    const createCustomerDto: CreateCustomerDto = {
-      userId: user.id,
-      ...createUserDto.customer
-    };
-    
+    const user = await this.create(createUserDto, Role.CUSTOMER);
     return user;
   }
 
   // Registrar un nuevo Vet 
   async registerVet(createUserDto: CreateUserDto): Promise<User> {
-    const user = await this.create(createUserDto, Role.VET); 
-
-    const createVetDto: CreateVetDto = {
-      userId: user.id,
-      ...createUserDto.vet
-    };
+    const user = await this.create(createUserDto, Role.VET);
     return user;
   }
 
   // Registrar un nuevo Shelter 
   async registerShelter(createUserDto: CreateUserDto): Promise<User> {
-    const user = await this.create(createUserDto, Role.SHELTER); 
-
-    const createShelterDto: CreateShelterDto = {
-      userId: user.id,
-      ...createUserDto.shelter
-    };
+    const user = await this.create(createUserDto, Role.SHELTER);
     return user;
   }
 
@@ -90,22 +74,22 @@ export class UsersService {
 
   async remove(id: number) {
     const userExists = await this.usersRepository.findOne({ where: { id }, relations: ['vet', 'customer', 'shelter'] });
-    
+
     if (!userExists) {
       throw new NotFoundException(`Does not exist a user with id: ${id}`);
     }
-  
+
     if (userExists.vet) {
-      await this.vetService.remove(userExists.vet.id); 
+      await this.vetService.remove(userExists.vet.id);
     }
     if (userExists.customer) {
-      await this.customerService.remove(userExists.customer.id); 
+      await this.customerService.remove(userExists.customer.id);
     }
     if (userExists.shelter) {
-      await this.shelterService.remove(userExists.shelter.id); 
+      await this.shelterService.remove(userExists.shelter.id);
     }
-  
+
     return await this.usersRepository.remove(userExists);
   }
-  
+
 }
