17f047927ccc5bc1f51f9846102707ea8b6c59a3
JayGhz <jaykansas7@gmail.com>
2025-01-28 22:14:50 -0500

    feat: Enhance breeds service with validation and error handling; update create-breed DTO with required fields

--- src/breeds/dto/create-breed.dto.ts
+++ src/breeds/dto/create-breed.dto.ts
@@ -1,1 +1,14 @@
-export class CreateBreedDto {}
+import { IsNotEmpty } from "class-validator";
+import { Specie } from "src/shared/enums/specie.enum";
+
+export class CreateBreedDto {
+
+    @IsNotEmpty({ message: 'Name is required' })
+    name: string;
+
+    @IsNotEmpty({ message: 'Description is required' })
+    description: string;
+
+    @IsNotEmpty({ message: 'Specie is required' })
+    specie: Specie;
+}
