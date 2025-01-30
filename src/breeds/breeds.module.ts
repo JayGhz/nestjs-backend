ec974edc11dfe4c3ea86589b450df2292c875702
JayGhz <jaykansas7@gmail.com>
2025-01-29 22:26:48 -0500

    feat: Add role-based authentication to breeds controller; integrate AuthModule and TypeOrmModule in breeds module

--- src/breeds/breeds.module.ts
+++ src/breeds/breeds.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { BreedsService } from './breeds.service';
 import { BreedsController } from './breeds.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Breed } from './entities/breed.entity';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
   controllers: [BreedsController],
   providers: [BreedsService],
+  imports: [TypeOrmModule.forFeature([Breed]), AuthModule],
 })
 export class BreedsModule {}
