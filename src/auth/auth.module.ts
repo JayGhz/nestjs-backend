b43f7ea41a62ec96d7f93bd0396ba8d52c8879ef
JayGhz <jaykansas7@gmail.com>
2025-03-10 12:21:56 -0500

    feat: config jwt module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,20 +1,26 @@
 import { forwardRef, Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
-import { jwtConstants } from '@/auth/constants/jwt.constants';
 import { RoleGuard } from '@/auth/guards/role.guard';
 import { AuthGuard } from '@/auth/guards/auth.guard';
 import { UsersModule } from '@/users/users.module';
 import { SharedModule } from '@/shared/shared.module';
 import { AuthController } from '@/auth/auth.controller';
 import { AuthService } from '@/auth/auth.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   controllers: [AuthController],
   providers: [AuthService, AuthGuard, RoleGuard],
-  imports: [forwardRef(() => UsersModule), SharedModule, JwtModule.register({
-    secret: jwtConstants.secret,
-    signOptions: { expiresIn: '1h' },
-  })],
+  imports: [forwardRef(() => UsersModule), SharedModule, JwtModule.registerAsync
+    ({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        global: true,
+        signOptions: { expiresIn: '1d' },
+      }),
+      inject: [ConfigService],
+    })],
   exports: [AuthService, AuthGuard, RoleGuard, JwtModule],
 })
 export class AuthModule { }
